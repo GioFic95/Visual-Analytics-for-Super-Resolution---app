8f718f80ea8229f91759d72ea6b0cad781731b5c
Giovanni Ficarra <giovanni.ficarra@wsense.it>
2022-07-08 21:40:47 +0200

    credentials

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ server.wsgi_app = WhiteNoise(server.wsgi_app, root='static/')
 # https://developers.google.com/identity/protocols/oauth2/web-server#python
 client_secrets = json.loads(os.environ.get("client_secrets", None))
 print("client_secrets:", client_secrets)
-appflow = flow.Flow.from_client_config(
+appflow = flow.InstalledAppFlow.from_client_config(  # flow.Flow.from_client_config(
     client_secrets, scopes=["https://www.googleapis.com/auth/drive.readonly"]
 )
 appflow.run_local_server()
