a269a0d74b95a07fad28f0162fbcacc423965ba7
Giovanni Ficarra <giovanni.ficarra@wsense.it>
2022-09-01 17:15:50 +0200

    adding box plot ops

--- app.py
+++ app.py
@@ -47,10 +47,13 @@ curr_dfs = get_df(csv_all, types)
 compute_pca(curr_dfs, metrics)
 
 par = parallel_plot(curr_dfp.query(make_query(avg=True)))
-scat = scatter_plot(curr_dfs.query(make_query()), "ssim", "psnr_rgb", highlights)
-box = box_plot(curr_dfs.query(make_query()), "ssim")
+tmp_dfs = curr_dfs.query(make_query())
+scat = scatter_plot(tmp_dfs, "ssim", "psnr_rgb", highlights)
+box = box_plot(tmp_dfs, "ssim")
+del tmp_dfs
 metric_combos = [f"{m1} VS {m2}" for m1, m2 in itertools.combinations(metrics, 2)] + ["pca_x VS pca_y"]
-last_m12 = [None, None]
+boxmetrics = metrics + ["pca_x", "pca_y"]
+last_m123 = [None, None, None]
 
 div_title = html.Div(html.H1(title), style={"margin-top": 30, "margin-left": 30})
 
@@ -67,7 +70,7 @@ div_scatter = html.Div([
 ], className='row')
 
 div_box = html.Div(dcc.Graph(config={'displayModeBar': False, 'doubleClick': 'reset'},
-                             figure=box, id=f"my-graph-box", style={'height': 400}),
+                             figure=box, id=f"my-graph-box", style={'height': 600}),
                    className='row')
 
 dataset_label = html.Label("Training dataset:", style={'font-weight': 'bold', 'margin-bottom': 10})
@@ -82,7 +85,7 @@ compression_radio = dcc.RadioItems({"img": "Image Compression", "vid": "Video Co
                                    inputClassName="form-check-input", labelClassName="form-check-label")
 compression_div = html.Div([compression_label, compression_radio], className="col")
 
-metrics_label = html.Label("Metrics:", style={'font-weight': 'bold', "text-align": "center", 'margin-bottom': 10})
+metrics_label = html.Label("Scatter metrics:", style={'font-weight': 'bold', "text-align": "center", 'margin-bottom': 10})
 metrics_dd = dcc.Dropdown(
                 id="metrics-dropdown",
                 options=metric_combos,
@@ -91,11 +94,21 @@ metrics_dd = dcc.Dropdown(
 )
 metrics_div = html.Div([metrics_label, metrics_dd], className="col")
 
+boxmetric_label = html.Label("Box metric:", style={'font-weight': 'bold', "text-align": "center", 'margin-bottom': 10})
+boxmetric_dd = dcc.Dropdown(
+                id="boxmetric-dropdown",
+                options=boxmetrics,
+                value="ssim",
+                style={'width': '200px'}
+)
+boxmetric_div = html.Div([boxmetric_label, boxmetric_dd], className="col")
+
 count_label = html.Label("Number of items:", style={'font-weight': 'bold', 'margin-bottom': 10})
 count_field = html.Div(html.Label("Counting...", id="count_lab"), id="count_div")
 count_div = html.Div([count_label, count_field], className="col")
 
-div_buttons = html.Div([dataset_div, compression_div, metrics_div, count_div], className="row", style={"margin": 15})
+div_buttons = html.Div([dataset_div, compression_div, metrics_div, boxmetric_div, count_div],
+                       className="row", style={"margin": 15})
 
 app.layout = html.Div([div_title, div_parallel, div_buttons, div_scatter, div_box])
 
@@ -103,30 +116,34 @@ app.layout = html.Div([div_title, div_parallel, div_buttons, div_scatter, div_bo
 @app.callback(
     Output('my-graph-sp', 'figure'),
     Output('my-graph-pp', 'figure'),
+    Output('my-graph-box', 'figure'),
     Output('count_lab', 'children'),
     Input('metrics-dropdown', 'value'),
+    Input('boxmetrics-dropdown', 'value'),
     Input('dataset-radio', 'value'),
     Input('compression-radio', 'value'),
     Input('my-graph-pp', 'restyleData'),
     State('my-graph-sp', 'figure'),
-    State('my-graph-pp', 'figure')
+    State('my-graph-pp', 'figure'),
+    State('my-graph-box', 'figure'),
 )
-def update_sp(drop_mc, radio_ds, radio_cp, selection, old_scat, old_par):
+def update_sp(drop_mc, drop_box, radio_ds, radio_cp, selection, old_scat, old_par, old_box):
     trigger = ctx.triggered_id
     print("trigger:", trigger)
     if trigger is None:
-        return old_scat, old_par, str(len(curr_dfs.query(make_query())))
+        return old_scat, old_par, old_box, str(len(curr_dfs.query(make_query())))
     elif trigger == "my-graph-pp":
-        return update_sp_parallel(selection, old_scat, old_par)
+        return update_sp_parallel(selection, old_scat, old_par, old_box)
     else:
-        return update_sp_buttons(drop_mc, radio_ds, radio_cp)
+        return update_sp_buttons(drop_mc, drop_box, radio_ds, radio_cp)
 
 
-def update_sp_buttons(drop_mc, radio_ds, radio_cp):
+def update_sp_buttons(drop_mc, drop_box, radio_ds, radio_cp):
     print('update_sp', drop_mc, radio_ds, radio_cp)
 
     m1, m2 = str(drop_mc).split(" VS ")
-    last_m12[0:2] = m1, m2
+    last_m123[0:2] = m1, m2
+    last_m123[2] = drop_box
     queries["dataset"] = f"train == '{radio_ds}'" if radio_ds != "" else ""
     queries["compression"] = f"type == '{radio_cp}'" if radio_cp != "" else ""
     query_dfs = make_query()
@@ -139,14 +156,15 @@ def update_sp_buttons(drop_mc, radio_ds, radio_cp):
     new_scat.update_layout(margin=dict(l=20, r=20, t=20, b=20))
     print("constraint_ranges:", constraint_ranges)
     new_par = parallel_plot(updated_dfp, constraint_ranges)
-    return new_scat, new_par, str(len(updated_dfs))
+    new_box = box_plot(updated_dfs, drop_box)
+    return new_scat, new_par, new_box, str(len(updated_dfs))
 
 
-def update_sp_parallel(selection, old_scat, old_par):
+def update_sp_parallel(selection, old_scat, old_par, old_box):
     print("selection:", selection)
 
     if selection is None:
-        return old_scat, old_par, str(len(curr_dfs))
+        return old_scat, old_par, old_box, str(len(curr_dfs))
     else:
         curr_dims = old_par['data'][0].get('dimensions', [])
         dim = curr_dims[-1]
@@ -177,12 +195,12 @@ def update_sp_parallel(selection, old_scat, old_par):
 
         traces = [traces[i] for i in idxs]
 
-        m1, m2 = last_m12
+        m1, m2, _ = last_m123
         queries["parallel"] = f"category in {[t for t in traces]}"
         updated_df = curr_dfs.query(make_query())
         new_scat = scatter_plot(updated_df, m1, m2, highlights)
         print("constraint_ranges:", constraint_ranges)
-        return new_scat, old_par, str(len(updated_df))
+        return new_scat, old_par, old_box, str(len(updated_df))
 
 
 @app.callback(
